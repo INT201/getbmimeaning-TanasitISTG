1543be4b8b6dfab4c4f72e46c680addd6350fc42
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-09-01 00:22:58 +0700

    hi hi hi

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,18 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return bmi = weight / Math.pow(height, 2)
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  const bmi = calculateBMI(weight, height)
+  if (bmi < 18.5) {
+    return 'Underweight'
+  }
+  if (bmi <= 24.9) {
+    return 'Normal weight'
+  }
+  if (bmi < 25) {
+    return 'Overweight'
+  }
 }
 module.exports = getBMIMeaning
